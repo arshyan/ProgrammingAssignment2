8abd8904cb03201432886d8c2a34a52b75f8b6d0
arshyan <arshyan.asd@gmail.com>
2015-07-25 15:57:46 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,4 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
 inv <- NULL
@@ -20,7 +17,7 @@ inv <- NULL
 }
 
 
-## Write a short comment describing this function
+
 
 cacheSolve <- function(x, ...) {
        m <- x$getinv()
